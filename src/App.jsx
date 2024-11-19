04aa03f8baaa21fb14f98c4723b021c905b923bf
RohanGorla <rohangorla2010@gmail.com>
2024-11-20 00:05:29 +0530

    Add present open animation and button

--- src/App.jsx
+++ src/App.jsx
@@ -3,18 +3,64 @@ import audiobgm from "../src/assets/Revara_BGM.mp3";
 import "./App.css";
 
 function App() {
+  const [showCardCover, setShowCardCover] = useState(true);
+  const [showOpenPresentButton, setShowOpenPresentButton] = useState(true);
   const bgm = new Audio();
 
   return (
-    <div>
-      <button
-        onClick={() => {
-          bgm.src = audiobgm;
-          bgm.play();
-        }}
+    <div className="Card_Container">
+      <div className="Card_Cover">
+        <div
+          className={
+            showCardCover
+              ? "Card_Cover--Quadrant_TL"
+              : "Card_Cover--Quadrant_TL Card_Cover--Quadrant_TL--Open"
+          }
+        ></div>
+        <div
+          className={
+            showCardCover
+              ? "Card_Cover--Quadrant_TR"
+              : "Card_Cover--Quadrant_TR Card_Cover--Quadrant_TR--Open"
+          }
+        ></div>
+        <div
+          className={
+            showCardCover
+              ? "Card_Cover--Quadrant_BL"
+              : "Card_Cover--Quadrant_BL Card_Cover--Quadrant_BL--Open"
+          }
+        ></div>
+        <div
+          className={
+            showCardCover
+              ? "Card_Cover--Quadrant_BR"
+              : "Card_Cover--Quadrant_BR Card_Cover--Quadrant_BR--Open"
+          }
+        ></div>
+      </div>
+      <div
+        className={
+          showCardCover
+            ? "Card_Cover--Open_Button"
+            : showOpenPresentButton
+            ? "Card_Cover--Open_Button Card_Cover--Open_Button--Toggle"
+            : "Card_Cover--Open_Button--Inactive"
+        }
       >
-        Play
-      </button>
+        <button
+          onClick={() => {
+            // bgm.src = audiobgm;
+            // bgm.play();
+            setShowCardCover(false);
+            setTimeout(() => {
+              setShowOpenPresentButton(false);
+            }, 2300);
+          }}
+        >
+          Open Present!
+        </button>
+      </div>
     </div>
   );
 }
