4065254e1190125c86cddb46ecfaf402f4f07d01
RohanGorla <rohangorla2010@gmail.com>
2024-11-20 03:02:57 +0530

    Add name and name animation

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,24 @@ import "./App.css";
 function App() {
   const [showCardCover, setShowCardCover] = useState(true);
   const [showOpenPresentButton, setShowOpenPresentButton] = useState(true);
+  const [specialCharacter, setSpecialCharacter] = useState(-1);
+  const [name, setName] = useState("REVARA");
   const bgm = new Audio();
+
+  function startMainAnimation() {
+    setInterval(() => {
+      let index = -1;
+      const characterInterval = setInterval(() => {
+        if (index + 1 !== name.length) {
+          index += 1;
+          setSpecialCharacter(index);
+        } else {
+          setSpecialCharacter(-1);
+          clearInterval(characterInterval);
+        }
+      }, 150);
+    }, name.length * 150 + 2000);
+  }
 
   return (
     <div className="Card_Container">
@@ -55,6 +72,9 @@ function App() {
             setShowCardCover(false);
             setTimeout(() => {
               setShowOpenPresentButton(false);
+              setTimeout(() => {
+                startMainAnimation();
+              }, 1000);
             }, 2300);
           }}
         >
@@ -62,7 +82,23 @@ function App() {
         </button>
       </div>
       <div className="Card_Main">
-
+        <div className="Card_Name">
+          {Array.from(name).map((character, index) => {
+            return (
+              <span
+                key={index}
+                className={
+                  specialCharacter === index
+                    ? "Card_Name--Character Card_Name--SpecialCharacter"
+                    : "Card_Name--Character"
+                }
+              >
+                {character}
+              </span>
+            );
+          })}
+        </div>
+        <div className="Card_Photos"></div>
       </div>
     </div>
   );
